2445ed9ed634581afbb5959bbe00b4dc81655be3
Grace Scharf <grace.scharf@Graces-MacBook-Pro.local>
2021-03-31 13:39:28 -0400

    feat: create summarystats interface and make use of config for locales

--- components/SummaryStats/SummaryStats.tsx
+++ components/SummaryStats/SummaryStats.tsx
@@ -6,37 +6,52 @@ import styles from './SummaryStats.module.scss'
 export default function SummaryStats() {
 
   if(!summaryStats) {
-    return (<h1>Search a stock ticker</h1>)
+    return (<h2>Search a stock ticker</h2>)
+  }
+
+  const config = {
+    week52high: {
+      label: 'Highest Price (52 weeks)',
+      data: toUSD(summaryStats.week52high)
+    },
+    week52low: {
+      label: 'Lowest Price (52 weeks)',
+      data: toUSD(summaryStats.week52low)
+    },
+    avg30Volume: {
+      label: 'Average Volume (30 days)',
+      data: toUsNum(summaryStats.avg30Volume)
+    },
+    day30ChangePercent: {
+      label: 'Change Percentage (30 days)',
+      data: `${(summaryStats.day30ChangePercent * 100).toFixed(2)}%`
+    },
+    employees: {
+      label: 'Employees',
+      data: toUsNum(summaryStats.employees)
+    }
+  }
+
+  const rows = () => {
+    return Object.entries(config).map(([key, entry]) => {
+      return (
+        <tr>
+          <th>{entry.label}</th>
+          <td>{entry.data}</td>
+        </tr>
+      )
+    })
   }
 
   return (
     <div>
       <h2>{summaryStats.companyName}</h2>
-     <table className={styles.summaryStats}>
-       <caption>Summary</caption>
-       <tbody>
-        <tr>
-          <th>Highest Price (52 weeks)</th>
-          <td>{toUSD(summaryStats.week52high)}</td>
-        </tr>
-        <tr>
-          <th>Lowest Price (52 weeks)</th>
-          <td>{toUSD(summaryStats.week52low)}</td>
-        </tr>
-        <tr>
-          <th>Average Volume (30 days)</th>
-          <td>{toUsNum(summaryStats.avg30Volume)}</td>
-        </tr>
-        <tr>
-          <th>Change Percentage (30 days)</th>
-          <td>{`${(summaryStats.day30ChangePercent * 100).toFixed(2)}%`}</td>
-        </tr>
-        <tr>
-          <th>Employees</th>
-          <td>{toUsNum(summaryStats.employees)}</td>
-        </tr>
-       </tbody>
-     </table>
+      <table className={styles.summaryStats}>
+        <caption>Summary</caption>
+        <tbody>
+        {rows()}
+        </tbody>
+      </table>
     </div>
   )
 }
