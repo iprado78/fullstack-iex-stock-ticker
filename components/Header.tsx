39c61e0a7257ef4dd936e20fcfbd05d4483db71b
Grace Scharf <62032574+gracescharf@users.noreply.github.com>
2021-04-01 14:42:20 -0400

    feat: add us bank logo and update to us bank colors (#9)
    Co-authored-by: Grace Scharf <grace.scharf@Graces-MacBook-Pro.local>

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,9 @@ import styles from "../styles/Home.module.css"
 export default function Header({ title }: { title: string }) {
   return (
     <header className={styles.title}>
+      <div className={styles.imgWrapper}>
+        <img src="USBankLogo.jpeg" alt="US Bank Logo" width="200"/>
+      </div>
       <h1>{title}</h1>
     </header>
   )
